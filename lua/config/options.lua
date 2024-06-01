9233dff08dbbce07c473558829c43e62f1098120
RyuuyaS <thinhyolo1994@gmail.com>
2024-06-01 16:38:32 +0700

    Stop using copilot and add support for winglang

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -15,3 +15,21 @@ vim.api.nvim_create_autocmd("FileType", {
 if vim.g.neovide == true then
   vim.o.guifont = "Fira Code SemiBold:h14"
 end
+
+vim.filetype.add({
+  extension = {
+    w = "wing",
+  },
+})
+
+LaunchWing = function()
+  local client_id = vim.lsp.start_client({ cmd = { "wing", "lsp" } })
+  vim.lsp.buf_attach_client(0, client_id)
+end
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "wing", "w" },
+  callback = function()
+    LaunchWing()
+  end,
+})
